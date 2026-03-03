fe7b0e2837132076eedfbf4d2c844b95142facfd
romerodavilajm-arch <romerodavila.jm@gmail.com>
2026-03-03 14:33:42 -0600

    feat: add 'Superficie' section to PanelLateral and include 'metros_cuadrados' property in geojson files

--- src/components/mapa/PanelLateral.jsx
+++ src/components/mapa/PanelLateral.jsx
@@ -121,6 +121,7 @@ export default function PanelLateral({ feature, onClose }) {
 
               <SeccionInfo titulo="Organización" contenido={props.organizacion} />
               <SeccionInfo titulo="Ubicación" contenido={props.ubicacion} />
+              <SeccionInfo titulo="Superficie" contenido={props.metros_cuadrados != null ? `${props.metros_cuadrados} m²` : null} />
               <SeccionInfo titulo="Descripción" contenido={props.descripcion} />
               <SeccionInfo titulo="Horario" contenido={props.horario} />
               <SeccionInfo titulo="Contacto" contenido={props.contacto} />
